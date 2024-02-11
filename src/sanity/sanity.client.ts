f5994cf88bf2b416db4a30286e2daf25cb98d980
Wubshet Zeleke Genta <wubezeleke@gmail.com>
2024-02-11 08:39:46 +0300

    Fix: Update API Version for Sanity

--- src/sanity/sanity.client.ts
+++ src/sanity/sanity.client.ts
@@ -3,7 +3,7 @@ import { createClient, type ClientConfig } from "@sanity/client";
 const config: ClientConfig = {
   projectId: "vuhehstv",
   dataset: "production",
-  apiVersion: "2024-01-11",
+  apiVersion: "2024-02-11",
 };
 
 const client = createClient(config);
