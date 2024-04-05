8d2eb5b60e64a095a54d6d732ddc38e66c3690e3
mac <wubezeleke@gmail.com>
2024-04-05 14:49:18 +0300

    Fix: Update API Version for Sanity

--- src/sanity/sanity.client.ts
+++ src/sanity/sanity.client.ts
@@ -3,7 +3,7 @@ import { createClient, type ClientConfig } from "@sanity/client";
 const config: ClientConfig = {
   projectId: "vuhehstv",
   dataset: "production",
-  apiVersion: "2024-03-02",
+  apiVersion: "2024-04-05",
 };
 
 const client = createClient(config);
