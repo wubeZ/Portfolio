b5188d9d43bbaf9c357ed78ef532d092cac01951
mac <wubezeleke@gmail.com>
2024-04-05 14:48:48 +0300

    Fix: Remove the Glowing Cursor for small screen

--- src/app/components/GlowingCursor.tsx
+++ src/app/components/GlowingCursor.tsx
@@ -29,7 +29,7 @@ const GlowingCursor: React.FC = () => {
   return (
     <div
       ref={cursorRef}
-      className="w-12 h-12 rounded-full bg-gradient-to-br from-[#d8d8d8] via-transparent to-[#d8d8d8] opacity-50 absolute pointer-events-none mix-blend-screen z-50 shadow-glow transition-all ease-linear border-2 border-[#d8d8d8] border-opacity-50 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24 2xl:w-28 2xl:h-28 3xl:w-32 3xl:h-32"
+      className="hidden lg:block w-12 h-12 rounded-full bg-gradient-to-br from-[#d8d8d8] via-transparent to-[#d8d8d8] opacity-50 absolute pointer-events-none mix-blend-screen z-50 shadow-glow transition-all ease-linear border-2 border-[#d8d8d8] border-opacity-50 sm:w-12 sm:h-12 md:w-16 md:h-16 lg:w-20 lg:h-20 xl:w-24 xl:h-24 2xl:w-28 2xl:h-28 3xl:w-32 3xl:h-32"
     ></div>
   );
 };
